0852fed99ffadd8c7c74d832160938a62c098ad2
Dantesos33 <dantesos33@gmail.com>
2025-10-25 18:02:11 +0500

    Sidebar Created

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +116,23 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div key={i.title} className="flex flex-col gap-2">
+          <span className="hidden font-light text-gray-400 my-4 lg:block">{i.title}</span>
+          {i.items.map((item)=>(
+            <Link href={item.href} key={item.label} className="flex justify-center lg:justify-start items-center gap-4 py-2 text-gray-500">
+              <Image src={item.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
