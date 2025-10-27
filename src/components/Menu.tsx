f9111094ba643f3df90a399dea425d7621fbdefb
Dantesos33 <dantesos33@gmail.com>
2025-10-27 13:39:11 +0500

    Menu Hover Effect Added, Menu Change Based on Role Added

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { role } from "@/lib/data";
 
 const menuItems = [
   {
@@ -122,13 +123,23 @@ const Menu = () => {
     <div className="mt-4 text-sm">
       {menuItems.map((i) => (
         <div key={i.title} className="flex flex-col gap-2">
-          <span className="hidden font-light text-gray-400 my-4 lg:block">{i.title}</span>
-          {i.items.map((item)=>(
-            <Link href={item.href} key={item.label} className="flex justify-center lg:justify-start items-center gap-4 py-2 text-gray-500">
-              <Image src={item.icon} alt="" width={20} height={20} />
-              <span className="hidden lg:block">{item.label}</span>
-            </Link>
-          ))}
+          <span className="hidden font-light text-gray-400 my-4 lg:block">
+            {i.title}
+          </span>
+          {i.items.map((item) => {
+            if(item.visible.includes(role)){
+            return (
+              <Link
+                href={item.href}
+                key={item.label}
+                className="flex justify-center lg:justify-start items-center gap-4 py-2 md:px-2 text-gray-500 rounded-md hover:bg-schooSkyLight"
+              >
+                <Image src={item.icon} alt="" width={20} height={20} />
+                <span className="hidden lg:block">{item.label}</span>
+              </Link>
+            );
+          }
+          })}
         </div>
       ))}
     </div>
